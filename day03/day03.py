fb22661132491fb1fc26f4c962e094afdf373503
sotsoguk <sotsoguk@gmail.com>
2018-12-04 00:06:24 +0100

    day03 Map

--- day03/day03.py
+++ day03/day03.py
@@ -1,18 +1,23 @@
 import numpy as np
+import re
+
 with open('input.txt') as f:
     lines = f.readlines()
 
 inp = []
 rect = np.zeros((1000,1000))
-for line in lines:
-    tmp = line.split()
-    idd = int(tmp[0].strip('#'))
-    x,y = map(int,(tmp[2].strip(":").split(",")))
-    xr,yr = map(int,tmp[3].split("x"))
-    inp.append([idd,x,y,xr,yr])
+inpm = map(lambda s:map(int, re.findall(r'-?\d+',s)),lines) 
+for (idd, x, y, dx,dy) in inpm:
+    inp.append([idd,x,y,dx,dy])
+# for line in lines:
+#     tmp = line.split()
+#     idd = int(tmp[0].strip('#'))
+#     x,y = map(int,(tmp[2].strip(":").split(",")))
+#     xr,yr = map(int,tmp[3].split("x"))
+#     inp.append([idd,x,y,xr,yr])
 
 def part1():
-    for idd,x,y,dx,dy in inp:
+    for _,x,y,dx,dy in inp:
         rect[x:x+dx,y:y+dy] += 1
     return np.sum(rect>1)
 
